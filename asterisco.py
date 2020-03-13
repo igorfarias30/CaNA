68f292586c1728b8e4fc73225a2c39b2023261fd
igorfarias30 <igorfarias30@hotmail.com>
2020-03-12 22:32:32 -0300

    Computing the number of executions

--- asterisco.py
+++ asterisco.py
@@ -1,23 +1,26 @@
 import sys
 
+x = 0
 
-def asterisco(n: int) -> None:
+def asterisco(n: int) -> int:
+    global x
+
     if (n > 1):
-        for i in range(0, 7 * n):
-            pass
-        
-        asterisco(n//5)
-        asterisco(n//5)
-        asterisco(n//6)
-        asterisco(n//6)
-        asterisco(n//6)
-        asterisco(n//10)
+        for _ in range(0, 7*n):
+            x += 1
 
+        for _ in range(2):
+            asterisco(n//5)
+        
+        for _ in range(3):
+            asterisco(n//6)
+
+    return x
 
 
 if __name__ == "__main__":
     
     if len(sys.argv) > 1:
-        asterisco(int(sys.argv[1]))
+        print(asterisco(int(sys.argv[1])))
     else:
         print("too many arguments. try: python asterisco.py <N>")
